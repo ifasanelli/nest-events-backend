dc8251e0e20bec5d97c222addba50e216a3cda68
Italo Miranda <italo.miranda@locaweb.tech>
2024-11-13 17:13:48 -0300

    x - responses and status codes

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,23 +1,32 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body } from "@nestjs/common"
+import { Controller, Get, Post, Patch, Delete, Param, Body, HttpCode } from "@nestjs/common"
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() { }
+  findAll() {
+    return [
+      { id: 1, name: 'First event' },
+      { id: 2, name: 'Second event' },
+      { id: 3, name: 'Third event' }
+    ]
+  }
+
   @Get(':id')
   findOne(@Param('id') id) {
-    return id;
+    return { id: 1, name: 'First event' };
   }
+
   @Post()
   create(@Body() input) {
     return input;
   }
+
   @Patch(':id')
   update(@Param('id') id, @Body() input) {
-    return id;
+    return input;
   }
+
   @Delete(':id')
-  remove(@Param('id') id) {
-    return id;
-  }
+  @HttpCode(204)
+  remove(@Param('id') id) {}
 }
