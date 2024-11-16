78aa9a2e32978de94e06cb3422babd863d1dbc9e
Italo Miranda <italo.miranda@locaweb.tech>
2024-11-16 15:23:46 -0300

    xxv -  repository querying criteria and options

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Patch, Delete, Param, Body, HttpCode } from "@ne
 import { CreateEventDto } from "./create-event.dto";
 import { UpdateEventDto } from "./update-event.dto";
 import { Event } from "./event.entity";
-import { Repository } from "typeorm";
+import { ILike, MoreThan, Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 
 @Controller('/events')
@@ -15,6 +15,25 @@ export class EventsController {
   @Get()
   async findAll() {
     return await this.repository.find();
+  }
+
+  @Get('/practice')
+  async practice() {
+    // https://typeorm.io/find-options
+    return await this.repository.find({
+      select: ['id', 'when'],
+      where: [{
+        id: MoreThan(3),
+        when: MoreThan(new Date('2021-02-12T13:00:00'))
+      }, {
+        description: ILike('%meet%')
+      }],
+      take: 2,
+      skip: 2,
+      order: {
+        id: 'DESC'
+      }
+    }); // SELECT id, when FROM event WHERE (event.id > 3 AND event.when > '2021-02-12T13:00:00') OR event.description ILIKE '%meet%' ORDER BY event.id DESC LIMIT 2 OFFSET 2
   }
 
   @Get(':id')
