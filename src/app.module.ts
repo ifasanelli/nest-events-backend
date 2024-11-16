ada26e2220ae71f936a8cf65537632c4fbca83db
Italo Miranda <italo.miranda@locaweb.tech>
2024-11-16 12:02:52 -0300

    xix - connecting to the database postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
+    type: 'postgres',
     host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
+    port: 5432,
+    username: 'postgres',
     password: 'example',
     database: 'nest-events'
   })],
